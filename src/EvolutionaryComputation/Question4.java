a623be7f8b09b5ba6c1926d0a418becc8a09a223
flzhang <flzhang@DESKTOP-UK43LT1>
2017-10-16 20:24:49 +0800

    增加了框架

--- src/EvolutionaryComputation/Question4.java
+++ src/EvolutionaryComputation/Question4.java
@@ -4,7 +4,9 @@ public class Question4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			PSO ts = new PSO();
+			ts.init();
+			ts.PSO_Method(10000);
 	}
 
 }
