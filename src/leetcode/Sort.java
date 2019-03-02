6f0e3acd4665e6179e45ce32d6bb50bcfa59a8a4
Flynn <1404009673@qq.com>
2019-03-02 09:25:32 +0800

    添加了归并排序的实现

--- src/leetcode/Sort.java
+++ src/leetcode/Sort.java
@@ -1,18 +1,25 @@
 package leetcode;
 
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.Merge;
+
 public class Sort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Sort  sort=new Sort();
-		int[] array= {6,5,4,3,7,7,2,1};
-		int[] array2= {1,2,3,4,5,6,7};
+		int[] array= {6,5,4,3,7,7,2,1,9,100};
+		int[] array2= {1,2,3,4,5,6,7,8};
 		int[] array3= {};
 //		sort.bubbleSort(array2, array2.length);
 //		sort.insertionSort(array,array.length); 
-		sort.selectionSort(array,array.length);
+//		sort.selectionSort(array,array.length);
+		sort.mergeSort(array, array.length);
+//		sort.merge(array2, 0, 3, 7);
 		for(int i=0;i<array.length;i++)
 		System.out.println(array[i]);
+		System.out.println(Arrays.toString(array));
 	}
 	//冒泡排序，时间复杂度为O(n2)，空间复杂度为O(1),是一种稳定的排序算法
 	public void bubbleSort(int[] a ,int n) {
@@ -78,8 +85,50 @@ public class Sort {
 			temp=a[i];
 			a[i]=a[k];
 			a[k]=temp;
+		}	
+	}
+	//归并排序，采用了分治算法的思想，类似于递归，递归是一种编程技巧，于是，采用分治算法的也需要写出递推公式和终止条件
+	//递推公式：merge_sort(p,r)=merge(merge_sort(p,q),merge_sort(q+1,r));
+	//终止条件：p>=r;
+	public void mergeSort(int[] a,int n) {
+		mergeSort_c(a, 0, n-1);
+	}
+	public void mergeSort_c(int[] a,int p,int r) {
+		if(p<r) {
+			mergeSort_c(a, p, (p+r)/2);
+			
+			mergeSort_c(a, ((p+r)/2+1), r);
+			merge(a, p, (p+r)/2, r);
 		}
-		
+	}
+	//二路归并 ,真的，变量命名太重要了。。。
+	public void merge(int[] a,int p,int q,int r) {
+		//先判断边界条件
+		int[] array=new int[r-p+1];//辅助数组
+		int i=p;
+		int j=q+1;
+		int k=0;  //三个辅助变量，游标
+		while(i<=q&&j<=r) {
+			if(a[i]<=a[j]) {
+				array[k++]=a[i++];
+			}else {
+				array[k++]=a[j++];
+			}	
+		}
+		if(i>q) {
+			while(j<=r) {
+				array[k++]=a[j++];
+			}
+		}else {
+			while(i<=q) {
+				array[k++]=a[i++];
+			}
+		}
+		k=0;
+		for(int x=p;x<=r;x++) {
+			a[x]=array[k];
+			k++;
+		}
 	}
 
 }
