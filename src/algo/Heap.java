e7bbb9eb0d33235bbf4e7ec6c34f41a06d707839
Flynn <1404009673@qq.com>
2019-03-19 22:08:27 +0800

    创建堆排序的框架

---
+++ src/algo/Heap.java
@@ -0,0 +1,37 @@
+package algo;
+
+public class Heap {
+	private int[] a; //堆存储的数组
+	private int n;   // 堆的大小
+	private int count; //堆中已有元素数量
+	public Heap(int capacity) {
+		a=new int[capacity];
+		n=capacity;
+		count=0;	
+	}
+	//往堆中插入一个元素
+	public  void insert(int data) {
+		
+	}
+	//删除堆顶元素
+	public  void  removeMax() {
+		
+	}
+	//自上往下堆化
+	public void heapify(int[] a,int n,int i) {
+		
+	}
+	// 在数组a中交换下标为 i,j的元素的值
+	public void  swap(int[]a,int i,int j) {
+		
+	}
+	//建堆操作
+	public static void buildHeap() {
+		
+	}
+	//堆排序
+	public  void  sort(int[] a,int n) {
+		
+	}
+
+}
