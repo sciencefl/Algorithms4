38fbe07e5f7ae4ee9a8037038d1e27b3bac824d8
Flynn <1404009673@qq.com>
2019-05-22 21:45:18 +0800

    增加了二叉树的题目

--- src/jianzhiOffer/Solution11_19.java
+++ src/jianzhiOffer/Solution11_19.java
@@ -1,12 +1,18 @@
 package jianzhiOffer;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+import edu.princeton.cs.algs4.Stack;
 
 public class Solution11_19 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Solution11_19 solution11_19=new Solution11_19();
+		TreeNode root=null;
+		ArrayList<Integer> arrayList=solution11_19.PrintFromTopToBottom(root);
+		System.out.println(arrayList);
 	}
 	/**
 	 * 镜像二叉树
@@ -55,5 +61,77 @@ public class Solution11_19 {
         }
         return arrayList;
     }
-
+    /**
+     * 包含
+     */
+    Stack<Integer> stack1= new Stack<>();
+    Stack<Integer> stackMin=new Stack<>(); 
+    public void push(int node) {
+        stack1.push(node);
+        if(stackMin.isEmpty()){
+            stackMin.push(node);
+        }else{
+            int min=stackMin.peek();
+            if(node<min){
+                stackMin.push(node);
+            }else {
+                stackMin.push(min);
+            }
+        }
+    }
+    public void pop() {
+       stack1.pop();
+       stackMin.pop();
+    }
+    
+    public int top() {
+        return stack1.peek();
+    }
+    
+    public int min() {
+        return stackMin.peek();
+    }
+    /**
+     * 栈的压入、弹出序列
+     * @param pushA
+     * @param popA
+     * @return
+     */
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+    	if(pushA==null|| pushA.length==0) {
+    		return false;
+    	}
+    	if(popA==null||popA.length==0) {
+    		return true;
+    	}
+    	Stack<Integer> dataStack=new Stack<>();
+    	int j=0;
+    	for(int i=0;i<pushA.length;i++) {
+    		dataStack.push(pushA[i]);
+    		while(dataStack.peek()==popA[j]) {
+    			dataStack.pop();
+    			j++;
+    		}
+    	}
+    	return dataStack.isEmpty();
+    }
+    /**
+     *  从上往下打印二叉树（二叉树的层次遍历）
+     */
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+    	LinkedList<TreeNode> linkedlist =new LinkedList();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        linkedlist.add(root);
+        while(!linkedlist.isEmpty()) {
+        	TreeNode tempNode=linkedlist.poll();
+        	arrayList.add(tempNode.val);
+        	if(tempNode.left!=null) {
+        		linkedlist.add(tempNode.left);
+        	}
+        	if(tempNode.right!=null) {
+        		linkedlist.add(tempNode.right);
+        	}
+        }
+        return arrayList;
+    }
 }
